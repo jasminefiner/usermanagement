54cfea0ea19d042808c3a1ff3dfa265a8f7c1b73
Jasmine Finer <jasminefiner@me.com>
2018-09-17 19:21:53 +0100

    basic flask application

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    app.config.from_object(config[config_name])
+
+    bootstrap.init_app(app)
+    
+    from .main_blueprint import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
